cd54c53f14e0b4bd9c8957d5b89b8048b3e31750
solnijko <fannomas88@gmail.com>
2023-11-15 22:54:55 +0300

    Added nav section

---
+++ src/section-top/TopSection.tsx
@@ -0,0 +1,27 @@
+import './TopSection.css'
+import '../index.css'
+
+function TopSection() {
+  return (
+    <>
+      <header>
+        <nav>
+          <div className="nav-top">
+            <button className="icon menu-btn"></button>
+            <a href="#" className="mainpage-link">Tech Heim</a>
+            <div className="top-right">
+              <button className="icon busket"></button>
+              <button className="icon user"></button>
+            </div>
+          </div>
+          <div className="search-box">
+            <input type="text" name="search" id="search" className='search' placeholder='What can we help you to find ?'/>
+            <button type="submit" className='icon search-btn'></button>
+          </div>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default TopSection
